2689d403f7a643ff6207a947e25c973ddf4dce3f
Lisbeth Diaz <lizbeths.robles@gmail.com>
2024-01-03 08:00:06 -0400

    chapter05 added

---
+++ Chapter05/PeopleApp/Program.Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+partial class Program
+{
+
+    private static void ConfigureConsole(string culture = "en-US", bool useComputerCulture = false, bool showCulture = false)
+    {
+        OutputEncoding = System.Text.Encoding.UTF8;
+        if (!useComputerCulture)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo(culture);
+        }
+        if (showCulture)
+        {
+            WriteLine($"Current culture: {CultureInfo.CurrentCulture.DisplayName}.");
+        }
+    }
+            
+}
