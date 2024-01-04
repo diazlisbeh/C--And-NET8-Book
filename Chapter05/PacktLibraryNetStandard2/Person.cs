631025ba51ab1045fff5bb57d1dece0534bcd845
Lisbeth Diaz <lizbeths.robles@gmail.com>
2024-01-04 13:05:37 -0400

    a ramdom commit

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -6,4 +6,10 @@ namespace Packt.Shared;
 
 public class Person
 {
+#region Fields
+  public string? Name;
+  public DateTime Born;
+  public WondersOfTheAncientWorld FavoriteAncientWonder;
+
+#endregion 
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -3,4 +3,6 @@
 ConfigureConsole();
 
 Person bob = new Person();
+bob.Name = "Bob";
+bob.FavoriteAncientWonder.ColossusOfRhodes;
 WriteLine(bob);
