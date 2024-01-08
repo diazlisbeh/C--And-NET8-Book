fa1b1f98f483ada52e5a656a491625c8d7fbb58d
Lisbeth Diaz <69221741+diazlisbeh@users.noreply.github.com>
2024-01-08 21:55:40 +0000

    Pattern matching with objects

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -22,3 +22,26 @@ WriteLine($"After: a={a}, b={b}, c={c}, d={d}");
 
 
 WriteLine(bob.GetFruit().Item2);
+
+Passenger[] passengers = {
+  new FirstClassPassenger { AirMiles = 1_419, Name = "Suman" },
+  new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
+  new BusinessClassPassenger { Name = "Janice" },
+  new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+  new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" },
+};
+
+foreach(Passenger pass in passengers){
+    decimal flighCost = pass switch
+    {
+        FirstClassPassenger p when p.AirMiles > 35_000 => 1_500M,
+        FirstClassPassenger p when p.AirMiles > 15_000 => 1_750M,
+         FirstClassPassenger _                         => 2_000M,
+    BusinessClassPassenger _                      => 1_000M,
+    CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M, 
+    CoachClassPassenger _                         => 650M,
+    _                                             => 800M
+  };
+  WriteLine($"Flight costs {flightCost:C} for {passenger}");
+    }
+}
