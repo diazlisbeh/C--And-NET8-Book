a1789d06d415dbd7c24c5a968c2221246b4f83d9
Lisbeth Diaz <69221741+diazlisbeh@users.noreply.github.com>
2024-01-19 21:54:13 +0000

    managing files and directories

--- Chapter09/WorkingWithFileSystem/Program.cs
+++ Chapter09/WorkingWithFileSystem/Program.cs
@@ -54,3 +54,61 @@ foreach (DriveInfo drive in DriveInfo.GetDrives())
   }
 }
 AnsiConsole.Write(drives);
+
+
+#region Managing Directories
+SectionTitle("Managing Directories");
+
+string newFolder = Combine(GetFolderPath(SpecialFolder.Personal),"newfolder");
+WriteLine($"Does Exits: {Path.Exists(newFolder)}");
+WriteLine("Creating ...");
+CreateDirectory(newFolder);
+
+// Let's use the Directory.Exists method this time.
+WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+Write("Confirm the directory exists, and then press any key.");
+ReadKey(intercept: true);
+WriteLine("Deleting it...");
+Delete(newFolder, recursive: true);
+WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+#endregion 
+
+#region Managing Files
+SectionTitle("Managing Files");
+
+string dir = Combine(GetFolderPath(SpecialFolder.Personal),"OutputFiles");
+CreateDirectory(dir);
+string textFile= Combine(dir,"Dummy.txt");
+string backupFile = Combine(dir,"Dummy.bak");
+WriteLine($"Working with: {textFile}");
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+StreamWriter textWriter = File.CreateText(textFile);
+textWriter.WriteLine("Heloo, C#");
+textWriter.Close();
+
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+File.Copy(sourceFileName: textFile,destFileName:backupFile,overwrite:true);
+ReadKey(true);
+WriteLine(
+  $"Does {backupFile} exist? {File.Exists(backupFile)}");
+Write("Confirm the files exist, and then press any key.");
+ReadKey(intercept: true);
+// Delete the file.
+File.Delete(textFile);
+WriteLine($"Does it exist? {File.Exists(textFile)}");
+// Read from the text file backup.
+WriteLine($"Reading contents of {backupFile}:");
+StreamReader textReader = File.OpenText(backupFile); 
+WriteLine(textReader.ReadToEnd());
+textReader.Close();
+#endregion
+
+
+SectionTitle("Getting file information");
+FileInfo info = new(backupFile);
+WriteLine($"{backupFile}:");
+WriteLine($"  Contains {info.Length} bytes.");
+WriteLine($"  Last accessed: {info.LastAccessTime}");
+WriteLine($"  Has readonly set to {info.IsReadOnly}.");
